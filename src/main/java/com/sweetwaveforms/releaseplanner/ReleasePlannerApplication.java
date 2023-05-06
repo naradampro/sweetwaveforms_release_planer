00cf3f6e847d4274bfff55672030c13decb81316
Narada Abeykoon <naradampro@gmail.com>
2023-05-06 18:13:55 +0530

    fix package name error by renaming

--- src/main/java/com/sweetwaveforms/releaseplanner/ReleasePlannerApplication.java
+++ src/main/java/com/sweetwaveforms/releaseplanner/ReleasePlannerApplication.java
@@ -1,4 +1,4 @@
-package com.sweetwaveform.releaseplanner;
+package com.sweetwaveforms.releaseplanner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/sweetwaveforms/releaseplanner/ReleasePlannerApplicationTests.java
+++ src/test/java/com/sweetwaveforms/releaseplanner/ReleasePlannerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.sweetwaveform.releaseplanner;
+package com.sweetwaveforms.releaseplanner;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
