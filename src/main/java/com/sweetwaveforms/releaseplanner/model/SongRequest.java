a90e70ef264c54ff02bfa306004986e28cba4e02
Narada Abeykoon <naradampro@gmail.com>
2023-05-07 17:38:32 +0530

    add constructors, getters and setters for SongRequest model

--- src/main/java/com/sweetwaveforms/releaseplanner/model/SongRequest.java
+++ src/main/java/com/sweetwaveforms/releaseplanner/model/SongRequest.java
@@ -26,4 +26,62 @@ public class SongRequest {
 
     @Column(name = "created_date")
     private LocalDateTime createdDate;
+
+    public SongRequest() {
+
+    }
+
+    public SongRequest(String title, String artistName, String youtubeLink) {
+        this.title = title;
+        this.artistName = artistName;
+        this.youtubeLink = youtubeLink;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+    public String getYoutubeLink() {
+        return youtubeLink;
+    }
+
+    public void setYoutubeLink(String youtubeLink) {
+        this.youtubeLink = youtubeLink;
+    }
+
+    public SongRequestStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(SongRequestStatus status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
 }
