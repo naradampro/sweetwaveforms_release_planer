ef6e3dc421d58ece3f327b87e36e03228f3df5fe
waokwarachchi <82043184+waokwarachchi@users.noreply.github.com>
2023-07-05 10:33:59 +0530

    WorkManager model updated

--- src/main/java/com/sweetwaveforms/releaseplanner/model/WorkManager.java
+++ src/main/java/com/sweetwaveforms/releaseplanner/model/WorkManager.java
@@ -34,7 +34,69 @@ public class WorkManager {
     @CreationTimestamp
     private LocalDateTime approvedDateTime;
 
+    public WorkManager() {
 
+    }
 
+    public WorkManager(String status, String approvedBy, WorkType workType) {
+        this.status = status;
+        this.approvedBy = approvedBy;
+        this.workType = workType;
+    }
 
+    public long getAssigneeId() {
+        return assigneeId;
+    }
+
+    public void setAssigneeId(long assigneeId) {
+        this.assigneeId = assigneeId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getApprovedBy() {
+        return approvedBy;
+    }
+
+    public void setApprovedBy(String approvedBy) {
+        this.approvedBy = approvedBy;
+    }
+
+    public WorkType getWorkType() {
+        return workType;
+    }
+
+    public void setWorkType(WorkType workType) {
+        this.workType = workType;
+    }
+
+    public LocalDateTime getAssignedDateTime() {
+        return assignedDateTime;
+    }
+
+    public void setAssignedDateTime(LocalDateTime assignedDateTime) {
+        this.assignedDateTime = assignedDateTime;
+    }
+
+    public LocalDateTime getDeadlineDateTime() {
+        return deadlineDateTime;
+    }
+
+    public void setDeadlineDateTime(LocalDateTime deadlineDateTime) {
+        this.deadlineDateTime = deadlineDateTime;
+    }
+
+    public LocalDateTime getApprovedDateTime() {
+        return approvedDateTime;
+    }
+
+    public void setApprovedDateTime(LocalDateTime approvedDateTime) {
+        this.approvedDateTime = approvedDateTime;
+    }
 }
