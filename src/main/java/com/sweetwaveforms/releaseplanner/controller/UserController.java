9803d1578af2eda9a854776e0af159813c92e835
Ayodhya Subhasiri <mdasubhasiri@students.nsbm.ac.lk>
2023-07-06 22:07:15 +0530

    changes port 9092 to 3000

--- src/main/java/com/sweetwaveforms/releaseplanner/controller/UserController.java
+++ src/main/java/com/sweetwaveforms/releaseplanner/controller/UserController.java
@@ -10,7 +10,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = "http://localhost:9092")
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/")
 public class UserController {
