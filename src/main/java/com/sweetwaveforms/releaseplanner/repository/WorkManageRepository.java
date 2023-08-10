bee61d521f6f7ad1d494bca30657c9303cf7217b
waokwarachchi <82043184+waokwarachchi@users.noreply.github.com>
2023-08-10 16:41:19 +0530

    workmanager repository

--- src/main/java/com/sweetwaveforms/releaseplanner/repository/WorkManageRepository.java
+++ src/main/java/com/sweetwaveforms/releaseplanner/repository/WorkManageRepository.java
@@ -3,5 +3,5 @@ package com.sweetwaveforms.releaseplanner.repository;
 import com.sweetwaveforms.releaseplanner.model.WorkManager;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface WorkManageRepository extends JpaRepository<WorkManager,long> {
+public interface WorkManageRepository extends JpaRepository<WorkManager,Long> {
 }
